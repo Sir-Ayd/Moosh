d089fc58e0d41515624e120db0d3575ffd6c10b5
Ayd <ayd@wildnetwork.co.uk>
2024-05-23 18:04:11 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const { token } = process.env;
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const g = GatewayIntentBits;
+const fs = require("fs");
+const client = new Client({
+  allowedMentions: {
+    parse: ['users', 'roles'], repliedUser: true
+  },
+  intents: [
+    g.Guilds,
+    g.GuildMembers,
+    g.GuildModeration,
+    g.GuildEmojisAndStickers,
+    g.GuildIntegrations,
+    g.GuildWebhooks,
+    g.GuildInvites,
+    g.GuildVoiceStates,
+    g.GuildPresences,
+    g.GuildMessages,
+    g.GuildMessageReactions,
+    g.GuildMessageTyping,
+    g.DirectMessageTyping,
+    g.DirectMessageReactions,
+    g.DirectMessages,
+    g.MessageContent,
+    g.GuildScheduledEvents,
+    g.DirectMessages,
+  ],
+});
+client.commands = new Collection();
+client.buttons = new Collection();
+client.selectMenus = new Collection();
+client.modals = new Collection();
+client.commandArray = [];
+
+client.on("error", () => { });
+
+const funtionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of funtionFolders) {
+  const funtionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of funtionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
+
+client.handleEvents();
+client.handleCommands();
+client.handleComponents();
+client.login(token);
