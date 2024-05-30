2b615e2336818a68dfa4cf801b39c018f43bceba
Taheemmi <135760363+Taheemmi@users.noreply.github.com>
2024-05-30 16:06:01 +0100

    Update Menu.js

--- src/client/src/components/Menu.js
+++ src/client/src/components/Menu.js
@@ -2,17 +2,19 @@
 
 import React from 'react';
 
-function Menu({ onStartClick, onSettingsClick }) {
+function Menu({ onStartClick, onSettingsClick, onBarnClick }) {
   return (
     <>
-    <div className='menu-background'>
-    <h1 className='header-menu'> COW TIPPER!</h1>
-    <div className='centre-menu'>
-      <h1>Main Menu</h1>
-      <button className='menu-buttons' onClick={onStartClick}>Start Game</button>
-      <button className='menu-buttons' onClick={onStartClick}>Barn</button>
-      <button className='menu-buttons' onClick={onSettingsClick}>Settings</button>
-    </div></div></>
+      <div className='menu-background'>
+        <h1 className='header-menu'>COW TIPPER!</h1>
+        <div className='centre-menu'>
+          <h1>Main Menu</h1>
+          <button className='menu-buttons' onClick={onStartClick}>Start Game</button>
+          <button className='menu-buttons' onClick={onBarnClick}>Barn</button> {/* Updated this line */}
+          <button className='menu-buttons' onClick={onSettingsClick}>Settings</button>
+        </div>
+      </div>
+    </>
   );
 }
 
