d5f5f4feb370ea9c31cb747938c9dd0237d67a4b
Ayd <ayd@wildnetwork.co.uk>
2024-05-30 12:30:10 +0100

    Menu component

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,14 +2,9 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import Game from './game/base';
-<<<<<<< Updated upstream
-import Settings from './game/OptionsPage'; // Import the Settings component
-import './index.css'; // change if CSS has been relocated
+import Menu from './components/Menu'
+import OptionsPage from './game/OptionsPage'; // Import the Settings component
 
-=======
-import OptionsPage from './game/OptionsPage';
-import Menu from './components/Menu'; // Import the Menu component
->>>>>>> Stashed changes
 
 function App() {
   const canvasRef = useRef(null);
@@ -48,40 +43,13 @@ function App() {
     setShowMenu(false);
   };
 
-<<<<<<< Updated upstream
   //function to go back a notch
-=======
->>>>>>> Stashed changes
   const handleBackClick = () => {
     setShowOptions(false);
     setShowMenu(true);
   };
 
   return (
-<<<<<<< Updated upstream
-
-    <div className={`App ${!showMenu ? 'background-hidden' : ''}`}>
-      <div className="background-container">
-        <div className="background-image"></div>
-      </div>
-      {showMenu ? (
-        <div className='centre-menu'>
-
-          <h3>Main Menu</h3>
-          <button className='menu-buttons' onClick={handleStartClick}>Start Game</button>
-          <button className='menu-buttons' onClick={handleStartClick}>Barn</button>
-          <button className='menu-buttons' onClick={handleSettingsClick}>Settings</button> {/* Call handleSettingsClick for "Settings" button */}
-        </div>
-        
-
-      ) : null}
-
-      {showOptions ? (
-        <div>
-          <Settings onBackClick={handleBackClick} /> 
-        </div>
-      ) : null}
-=======
     <div className="App">
       {showMenu && (
         <Menu onStartClick={handleStartClick} onSettingsClick={handleSettingsClick} />
@@ -90,7 +58,6 @@ function App() {
       {showOptions && (
         <OptionsPage onBackClick={handleBackClick} />
       )}
->>>>>>> Stashed changes
 
       <canvas ref={canvasRef} style={{ border: '1px solid black', display: showMenu ? 'none' : 'block' }} />
     </div>
